9d813c39778ab6e59ad46f55f7d7c0832676d7fb
zikiclass <ezekieledafe5@gmail.com>
2025-05-01 02:28:06 -0700

    updates

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,7 +16,11 @@ const metadata = {
     process.env.PROJECT_NAME +
     ": The Online Trading and Mining Platform - " +
     process.env.PROJECT_NAME,
-  description: process.env.PROJECT_DESCRIPTION,
+  description:
+    "Sign up With Trade Experts Market to join thousands of traders currently benefiting from high leveraged full STP/ECN CFD trading with zero conflict of interest through tier one liquidity.",
+  icons: {
+    icon: "/img/Trading.jpeg", // Path must be in `public/`
+  },
 };
 
 export default function RootLayout({ children }) {
@@ -53,17 +57,6 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
-      <head>
-        <link
-          rel="shortcut icon"
-          href="/img/Trading.jpeg"
-          type="image/x-icon"
-        />
-        <meta
-          name="description"
-          content="Sign up With Trade Experts Market to join thousands of traders currently benefiting from high leveraged full STP/ECN CFD trading with zero conflict of interest through tier one liquidity."
-        ></meta>
-      </head>
       <body className={signika.className}>
         <AuthProvider>
           <ThemeProvider attribute="class">
